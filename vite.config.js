fdb64329d562567b8f3c22cf883f48664fad97e9
Miftah Juanda <miftahjuanda21@gmail.com>
2025-05-02 21:46:45 +0700

    add base env

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), "@tailwindcss/postcss"],
+  base: process.env.VITE_BASE_PATH || "/document-verification-app",
 });
